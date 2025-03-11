18831437cb83a72da2cea4997e3fc9a070237373
Your Name <michalkacpernowak@gmail.com>
2025-03-11 19:20:17 +0100

    average

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,3 +4,13 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Modyfikacja 1!");
 Console.WriteLine("Modyfikacja 2!");
 Console.WriteLine("Modyfikacja 3!");
+
+static int Average(int[] numbers)
+{
+    int sum = 0;
+    foreach (int number in numbers)
+    {
+        sum += number;
+    }
+    return sum / numbers.Length;
+}
